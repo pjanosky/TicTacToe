e0135242d181105a5a9d1be913f5ee00ce846202
pjanosky <pcjanosky@gmail.com>
2020-01-05 22:11:18 -0500

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Peter Janosky on 1/5/20.
+//  Copyright © 2020 Peter Janosky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
